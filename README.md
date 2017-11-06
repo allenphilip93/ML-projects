0f5d46a89aaa903dc15758e5aef5f1c0c51ca91e
Allen Philip <allenphilip93@gmail.com>
2017-11-07 00:26:17 +0530

    #1: Basic linear regression on 1D dataset
     - Simple linear regression to train the model on the dataset
     - Get the RMS error for the same after predicting on the dataset
     - Plot the same to visualise the predictions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ML-projects
 Small ML projects
+
+Linear Regression:
+
+* Run pip install -r requirements.txt --user to install the dependencies.
+* Run python linear-reg.py to run linear regression on the dataset.
---
+++ linear-regression/linear-reg.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+from math import sqrt
+
+# import the dataset
+dataframe = pd.read_csv('dataset1.txt', header=None)
+x_values = dataframe[0].reshape(len(dataframe[0]), 1)
+y_values = dataframe[1]
+
+# train model on data
+body_reg = linear_model.LinearRegression()
+body_reg.fit(x_values, y_values)
+
+# evaluate the error
+y_predict = body_reg.predict(x_values)
+y_err = sqrt(mean_squared_error(y_predict, y_values))
+print "RMS Error of Model: " + str(y_err)
+
+# visualize the results
+plt.scatter(x_values, y_values, color='red')
+plt.plot(x_values, body_reg.predict(x_values), color='blue', linewidth=3)
+plt.show()
