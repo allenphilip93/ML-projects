b86030a68fa7b4ed0235cd618f597cb8d61a6671
Allen Philip John <allenphilip.john@jda.com>
2017-11-10 17:05:03 +0530

    Updating the dataset to something more relevant for the impl of NN

--- 03-neural-network/neural-network.py
+++ 03-neural-network/neural-network.py
@@ -1,7 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class NeuralNetwork:
-	def __init__(self):
+	def __init__(self, num_attr, num_neurons):
 		# seed the random generator to give same numbers every run
 		np.random.seed(1)
 
@@ -9,7 +10,10 @@ class NeuralNetwork:
 		# and 1 output connection
 
 		# Weights will be assigned to a 3x1 matrix with values [-1, 1] mean 0
-		self.synaptic_weights = 2 * np.random.random((3,1)) - 1
+		self.synaptic_weights = 2 * np.random.random((num_attr, num_neurons)) - 1
+
+		# error logs
+		self.mean_error_log = []
 
 	# Activation function to normalize the results to [-1, 1]
 	def __sigmoid(self, x):
@@ -25,37 +29,55 @@ class NeuralNetwork:
 			model_output = self.think(training_input)
 
 			# Calculate the error 
-			error = training_output - model_output.T
-
+			error = training_output - model_output
+			
 			# Gradient descent for weight adjustments
-			adj = np.dot(training_input.T, self.__sigmoid_derivative(model_output) * error.T)
+			adj = np.dot(training_input.T, self.__sigmoid_derivative(model_output) * error)
 
 			# Adjust the weights
 			self.synaptic_weights += adj
 
-			print "Epoch #{0}: Error = {1}".format(iteration, sum(error))
+			# print "Epoch #{0}: Error = {1}".format(iteration+1, np.mean(error))
+			
+			self.mean_error_log.append(np.mean(error))
+
 
 	# Output processing
 	def think(self, training_input):
 		# Pass inputs through our neural network
 		return self.__sigmoid(np.dot(training_input, self.synaptic_weights))
 
+	# Plot the error logs
+	def plot_error(self):
+		print "Trained Weights: " + str(self.synaptic_weights)
+		plt.plot(range(len(self.mean_error_log)), self.mean_error_log, color='blue')
+		plt.show()
+
 
 def run():
 	# training data with input and output - price = func(rooms, baths, floors)
-	points = np.genfromtxt('kc_house_data.csv', delimiter=",", dtype=float)
-	num_iters = 10
+	points = np.genfromtxt('zoo_data.csv', delimiter=",", dtype=float)
+	# points = np.array([[0, 0, 1, 0], [1, 1, 1, 1], [1, 0, 1, 1], [0, 1, 1, 0]])
 
-	print "Loaded house price correlation dataset"
+	# Set basic [params
+	num_iters = 5000
+	num_attr = 14
+	num_neurons = 1
+
+	print "Loaded zoo dataset for animal type classification"
 
 	# Initialize the neural network
-	neural_network = NeuralNetwork()
+	neural_network = NeuralNetwork(num_attr, num_neurons)
 
 	# Random initializing the synaptic weights
+	print "Initializing the synaptic weights"
 	print neural_network.synaptic_weights
 
 	# Traing the neural network
-	neural_network.train(points[:,1:], points[:,0], num_iters)
+	neural_network.train(points[:,:num_attr], points[:,num_attr:num_attr+1], num_iters)
+
+	# Plot error
+	neural_network.plot_error()
 
 
 if __name__ == "__main__":
